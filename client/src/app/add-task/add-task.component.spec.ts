bf43c534e00896ac61bac13d8c89ef95410776f5
Soumyadip375543Git <SOUMYADIP.HATI@COGNIZANT.COM>
2019-09-18 05:05:19 -0700

    init

--- client/src/app/add-task/add-task.component.spec.ts
+++ client/src/app/add-task/add-task.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AddTaskComponent } from './add-task.component';
+import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { Ng5SliderModule } from 'ng5-slider';
+import { BsDatepickerModule, ModalModule } from 'ngx-bootstrap';
+import { FilteruserPipe } from '../pipes/filteruser.pipe';
+import { EventService } from '../services/event.service';
+import { ProjectService } from '../services/project.service';
 
 describe('AddTaskComponent', () => {
   let component: AddTaskComponent;
@@ -8,7 +14,16 @@ describe('AddTaskComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddTaskComponent ]
+      imports:[FormsModule, Ng5SliderModule,BsDatepickerModule, 
+        ToastrModule.forRoot({
+        timeOut: 3000,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: true
+      }),],
+      declarations: [ AddTaskComponent, FilteruserPipe ],
+      providers:[EventService,
+        //HttpTestingModule,
+        ProjectService]
     })
     .compileComponents();
   }));
